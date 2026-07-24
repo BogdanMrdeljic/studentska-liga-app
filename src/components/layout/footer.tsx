10958b7501ccd71886b49ad685efe7c0baf32a79
BogdanMrdeljic <bogdanmrdeljic10032003@gmail.com>
2026-07-24 17:08:28 +0200

    Remove Aqua Viva sponsor from the footer
    Back to three sponsors (Yettel, Triglav, ArduSport), centered under
    the sponsors heading.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -35,15 +35,6 @@ export function Footer() {
               className="object-contain"
             />
           </div>
-          <div className="relative h-10 w-10">
-            <Image
-              src="/sponsors/aquaviva.png"
-              alt="Aqua Viva"
-              fill
-              sizes="40px"
-              className="object-contain"
-            />
-          </div>
         </div>
 
         <div className="mt-6 border-t pt-4 text-center text-sm text-muted-foreground">
